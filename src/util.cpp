3b06ea7dbd9ce9afbe9b17636c6d667c63f52411
Prithvi A Pai <prithvi.a.pai@intel.com>
2022-09-21 11:53:28 +0530

    Remove libspdm linking in spdmd (#14)
    spdmapplib is developed to provide abstraction to libspdm APIs.
    Currently in spdmd(spdm responder daemon) libspdm was linked for getting
    config related macros which were defined in libspdm.
    Current patch uses spdmapplib supported API getConfigMap to get the
    config related information from spdmapplib. Thus removing linking
    libspdm directly to spdmapplib.
    
    Tested:
    BMC as SPDM responder and Aardvark as requester and test passes
    
    Signed-off-by: Prithvi A Pai <prithvi.a.pai@intel.com>

--- src/util.cpp
+++ src/util.cpp
@@ -17,135 +17,13 @@
 #include "spdmapplib.hpp"
 
 #include <phosphor-logging/log.hpp>
-extern "C"
-{
-#include "library/spdm_common_lib.h"
-}
+
 #include <iostream>
 #include <map>
 
 using configurationField =
     std::variant<bool, uint64_t, std::string, std::vector<std::string>>;
 using ConfigurationMap = std::unordered_map<std::string, configurationField>;
-
-std::map<std::string, uint32_t> versionValueStringTable = {
-    {"1.0", SPDM_MESSAGE_VERSION_10},
-    {"1.1", SPDM_MESSAGE_VERSION_11},
-    {"1.2", SPDM_MESSAGE_VERSION_12},
-};
-
-std::map<std::string, uint32_t> securedMessageVersionValueStringTable = {
-    {"0", 0},
-    {"1.0", SPDM_MESSAGE_VERSION_10},
-    {"1.1", SPDM_MESSAGE_VERSION_11},
-};
-
-std::map<std::string, uint32_t> spdmRequesterCapabilitiesStringTable = {
-    {"CERT", SPDM_GET_CAPABILITIES_REQUEST_FLAGS_CERT_CAP},
-    {"CHAL", SPDM_GET_CAPABILITIES_REQUEST_FLAGS_CHAL_CAP},
-    {"ENCRYPT", SPDM_GET_CAPABILITIES_REQUEST_FLAGS_ENCRYPT_CAP},
-    {"MAC", SPDM_GET_CAPABILITIES_REQUEST_FLAGS_MAC_CAP},
-    {"MUT_AUTH", SPDM_GET_CAPABILITIES_REQUEST_FLAGS_MUT_AUTH_CAP},
-    {"KEY_EX", SPDM_GET_CAPABILITIES_REQUEST_FLAGS_KEY_EX_CAP},
-    {"PSK", SPDM_GET_CAPABILITIES_REQUEST_FLAGS_PSK_CAP_REQUESTER},
-    {"ENCAP", SPDM_GET_CAPABILITIES_REQUEST_FLAGS_ENCAP_CAP},
-    {"HBEAT", SPDM_GET_CAPABILITIES_REQUEST_FLAGS_HBEAT_CAP},
-    {"KEY_UPD", SPDM_GET_CAPABILITIES_REQUEST_FLAGS_KEY_UPD_CAP},
-    {"HANDSHAKE_IN_CLEAR",
-     SPDM_GET_CAPABILITIES_REQUEST_FLAGS_HANDSHAKE_IN_THE_CLEAR_CAP},
-    {"PUB_KEY_ID", SPDM_GET_CAPABILITIES_REQUEST_FLAGS_PUB_KEY_ID_CAP},
-    {"CHUNK", SPDM_GET_CAPABILITIES_REQUEST_FLAGS_CHUNK_CAP},
-};
-
-std::map<std::string, uint32_t> spdmResponderCapabilitiesStringTable = {
-    {"CACHE", SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_CACHE_CAP},
-    {"CERT", SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_CERT_CAP},
-    {"CHAL", SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_CHAL_CAP},
-    {"MEAS_NO_SIG", SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_MEAS_CAP_NO_SIG},
-    {"MEAS_SIG", SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_MEAS_CAP_SIG},
-    {"MEAS_FRESH", SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_MEAS_FRESH_CAP},
-    {"ENCRYPT", SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_ENCRYPT_CAP},
-    {"MAC", SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_MAC_CAP},
-    {"MUT_AUTH", SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_MUT_AUTH_CAP},
-    {"KEY_EX", SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_KEY_EX_CAP},
-    {"PSK", SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_PSK_CAP_RESPONDER},
-    {"PSK_WITH_CONTEXT",
-     SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_PSK_CAP_RESPONDER_WITH_CONTEXT},
-    {"ENCAP", SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_ENCAP_CAP},
-    {"HBEAT", SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_HBEAT_CAP},
-    {"KEY_UPD", SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_KEY_UPD_CAP},
-    {"HANDSHAKE_IN_CLEAR",
-     SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_HANDSHAKE_IN_THE_CLEAR_CAP},
-    {"PUB_KEY_ID", SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_PUB_KEY_ID_CAP},
-    {"CHUNK", SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_CHUNK_CAP},
-    {"ALIAS_CERT", SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_ALIAS_CERT_CAP},
-};
-
-std::map<std::string, uint32_t> hashValueStringTable{
-    {"SHA_256", SPDM_ALGORITHMS_BASE_HASH_ALGO_TPM_ALG_SHA_256},
-    {"SHA_384", SPDM_ALGORITHMS_BASE_HASH_ALGO_TPM_ALG_SHA_384},
-    {"SHA_512", SPDM_ALGORITHMS_BASE_HASH_ALGO_TPM_ALG_SHA_512},
-    {"SHA3_256", SPDM_ALGORITHMS_BASE_HASH_ALGO_TPM_ALG_SHA3_256},
-    {"SHA3_384", SPDM_ALGORITHMS_BASE_HASH_ALGO_TPM_ALG_SHA3_384},
-    {"SHA3_512", SPDM_ALGORITHMS_BASE_HASH_ALGO_TPM_ALG_SHA3_512},
-    {"SM3_256", SPDM_ALGORITHMS_BASE_HASH_ALGO_TPM_ALG_SM3_256},
-};
-
-std::map<std::string, uint32_t> measurementHashValueStringTable = {
-    {"RAW_BIT", SPDM_ALGORITHMS_MEASUREMENT_HASH_ALGO_RAW_BIT_STREAM_ONLY},
-    {"SHA_256", SPDM_ALGORITHMS_MEASUREMENT_HASH_ALGO_TPM_ALG_SHA_256},
-    {"SHA_384", SPDM_ALGORITHMS_MEASUREMENT_HASH_ALGO_TPM_ALG_SHA_384},
-    {"SHA_512", SPDM_ALGORITHMS_MEASUREMENT_HASH_ALGO_TPM_ALG_SHA_512},
-    {"SHA3_256", SPDM_ALGORITHMS_MEASUREMENT_HASH_ALGO_TPM_ALG_SHA3_256},
-    {"SHA3_384", SPDM_ALGORITHMS_MEASUREMENT_HASH_ALGO_TPM_ALG_SHA3_384},
-    {"SHA3_512", SPDM_ALGORITHMS_MEASUREMENT_HASH_ALGO_TPM_ALG_SHA3_512},
-    {"SM3_256", SPDM_ALGORITHMS_MEASUREMENT_HASH_ALGO_TPM_ALG_SM3_256},
-};
-
-std::map<std::string, uint32_t> asymValueStringTable = {
-    {"RSASSA_2048", SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_RSASSA_2048},
-    {"RSASSA_3072", SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_RSASSA_3072},
-    {"RSASSA_4096", SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_RSASSA_4096},
-    {"RSAPSS_2048", SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_RSAPSS_2048},
-    {"RSAPSS_3072", SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_RSAPSS_3072},
-    {"RSAPSS_4096", SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_RSAPSS_4096},
-    {"ECDSA_P256", SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_ECDSA_ECC_NIST_P256},
-    {"ECDSA_P384", SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_ECDSA_ECC_NIST_P384},
-    {"ECDSA_P521", SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_ECDSA_ECC_NIST_P521},
-    {"SM2_P256", SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_SM2_ECC_SM2_P256},
-    {"EDDSA_25519", SPDM_ALGORITHMS_BASE_ASYM_ALGO_EDDSA_ED25519},
-    {"EDDSA_448", SPDM_ALGORITHMS_BASE_ASYM_ALGO_EDDSA_ED448},
-};
-
-std::map<std::string, uint32_t> dheValueStringTable = {
-    {"FFDHE_2048", SPDM_ALGORITHMS_DHE_NAMED_GROUP_FFDHE_2048},
-    {"FFDHE_3072", SPDM_ALGORITHMS_DHE_NAMED_GROUP_FFDHE_3072},
-    {"FFDHE_4096", SPDM_ALGORITHMS_DHE_NAMED_GROUP_FFDHE_4096},
-    {"SECP_256_R1", SPDM_ALGORITHMS_DHE_NAMED_GROUP_SECP_256_R1},
-    {"SECP_384_R1", SPDM_ALGORITHMS_DHE_NAMED_GROUP_SECP_384_R1},
-    {"SECP_521_R1", SPDM_ALGORITHMS_DHE_NAMED_GROUP_SECP_521_R1},
-    {"SM2_P256", SPDM_ALGORITHMS_DHE_NAMED_GROUP_SM2_P256},
-};
-
-std::map<std::string, uint32_t> aeadValueStringTable = {
-    {"AES_128_GCM", SPDM_ALGORITHMS_AEAD_CIPHER_SUITE_AES_128_GCM},
-    {"AES_256_GCM", SPDM_ALGORITHMS_AEAD_CIPHER_SUITE_AES_256_GCM},
-    {"CHACHA20_POLY1305", SPDM_ALGORITHMS_AEAD_CIPHER_SUITE_CHACHA20_POLY1305},
-    {"SM4_128_GCM", SPDM_ALGORITHMS_AEAD_CIPHER_SUITE_AEAD_SM4_GCM},
-};
-
-std::map<std::string, uint32_t> basicMutAuthPolicyStringTable = {
-    {"NO", 0},
-    {"BASIC", 1},
-};
-
-std::map<std::string, uint32_t> mutAuthPolicyStringTable = {
-    {"NO", 0},
-    {"WO_ENCAP", SPDM_KEY_EXCHANGE_RESPONSE_MUT_AUTH_REQUESTED},
-    {"W_ENCAP",
-     SPDM_KEY_EXCHANGE_RESPONSE_MUT_AUTH_REQUESTED_WITH_ENCAP_REQUEST},
-    {"DIGESTS", SPDM_KEY_EXCHANGE_RESPONSE_MUT_AUTH_REQUESTED_WITH_GET_DIGESTS},
-};
 
 static const std::string spdmTypeName =
     "xyz.openbmc_project.Configuration.SPDMConfiguration";
@@ -291,6 +169,26 @@ API to get SPDM configuration from Entity Manager. Called by requester and
 responder.
 */
 
+template <typename T>
+bool getSPDMConfigData(T& spdmConfig, std::vector<std::string>& spdmUserConfig,
+                       const std::map<std::string, uint32_t> spdmConfigVerifier)
+{
+    T spdmConfigData;
+    for (std::string& entry : spdmUserConfig)
+    {
+        spdmConfigData = 0;
+
+        if (!getValueFromName(spdmConfigVerifier, entry, spdmConfigData))
+        {
+            spdmUserConfig.clear();
+            return false;
+        }
+        spdmConfig = spdmConfig | spdmConfigData;
+    }
+    spdmUserConfig.clear();
+    return true;
+}
+
 /**
  * @brief get configuration from entitymanager
  *
@@ -305,12 +203,14 @@ spdm_app_lib::SPDMConfiguration getConfigurationFromEntityManager(
     std::shared_ptr<sdbusplus::asio::connection> conn,
     const std::string& configurationName)
 {
-    uint32_t u32Data;
+    uint64_t slotCount = 0;
+    std::string spdmStrConfig{};
+    std::vector<std::string> spdmUserConfig{};
     spdm_app_lib::SPDMConfiguration spdmConfig{};
     const std::string objectPath =
         getSPDMConfigurationPaths(conn, configurationName);
 
-    ConfigurationMap map;
+    ConfigurationMap map{};
     if (objectPath.empty())
     {
         return spdmConfig;
@@ -324,153 +224,109 @@ spdm_app_lib::SPDMConfiguration getConfigurationFromEntityManager(
         return spdmConfig;
     }
 
-    // Get cert file path
-    std::string certPath;
-    if (getField(map, "CertPath", certPath))
+    if (getField(map, "CertPath", spdmStrConfig))
     {
-        spdmConfig.certPath = std::move(certPath);
+        spdmConfig.certPath = std::move(spdmStrConfig);
     }
     else
     {
         spdmConfig.certPath = "/usr/share/spdmd/sample_keys";
     }
 
-    // get capability
-    std::vector<std::string> capability;
-    if (!getField(map, "Capability", capability))
+    if (!getField(map, "Version", spdmStrConfig))
+    {
+        return spdmConfig;
+    }
+    if (!getValueFromName(spdm_app_lib::getSPDMConfigMap(
+                              spdm_app_lib::SPDMConfigIdentifier::version),
+                          spdmStrConfig, spdmConfig.version))
     {
         return spdmConfig;
     }
 
-    for (std::string& entry : capability)
+    if (!getField(map, "Capability", spdmUserConfig))
     {
-        u32Data = 0;
-
-        if (!getValueFromName(spdmResponderCapabilitiesStringTable, entry,
-                              u32Data))
-        {
-            return spdmConfig;
-        }
-        spdmConfig.capability = spdmConfig.capability | u32Data;
+        return spdmConfig;
     }
-
-    // get hash
-    std::vector<std::string> hash;
-    if (!getField(map, "Hash", hash))
+    if (!getSPDMConfigData(
+            spdmConfig.capability, spdmUserConfig,
+            spdm_app_lib::getSPDMConfigMap(
+                spdm_app_lib::SPDMConfigIdentifier::responderCaps)))
     {
         return spdmConfig;
     }
 
-    for (std::string& entry : hash)
+    if (!getField(map, "Hash", spdmUserConfig))
     {
-        u32Data = 0;
-        if (!getValueFromName(hashValueStringTable, entry, u32Data))
-        {
-            return spdmConfig;
-        }
-        spdmConfig.hash = spdmConfig.hash | u32Data;
+        return spdmConfig;
     }
-
-    // get meas hash
-    std::vector<std::string> measHash;
-    if (!getField(map, "MeasHash", measHash))
+    if (!getSPDMConfigData(spdmConfig.hash, spdmUserConfig,
+                           spdm_app_lib::getSPDMConfigMap(
+                               spdm_app_lib::SPDMConfigIdentifier::baseHash)))
     {
         return spdmConfig;
     }
 
-    for (std::string& entry : measHash)
+    if (!getField(map, "MeasHash", spdmUserConfig))
     {
-        u32Data = 0;
-        if (!getValueFromName(measurementHashValueStringTable, entry, u32Data))
-        {
-            return spdmConfig;
-        }
-        spdmConfig.measHash = spdmConfig.measHash | u32Data;
+        return spdmConfig;
     }
-
-    // get asym
-    std::vector<std::string> asym;
-    if (!getField(map, "Asym", asym))
+    if (!getSPDMConfigData(spdmConfig.measHash, spdmUserConfig,
+                           spdm_app_lib::getSPDMConfigMap(
+                               spdm_app_lib::SPDMConfigIdentifier::measHash)))
     {
         return spdmConfig;
     }
 
-    for (std::string& entry : asym)
+    if (!getField(map, "Asym", spdmUserConfig))
     {
-        u32Data = 0;
-        if (!getValueFromName(asymValueStringTable, entry, u32Data))
-        {
-            return spdmConfig;
-        }
-        spdmConfig.asym = spdmConfig.asym | u32Data;
+        return spdmConfig;
     }
-    // get reqasym
-    std::vector<std::string> reqasym;
-    if (!getField(map, "ReqAsym", reqasym))
+    if (!getSPDMConfigData(spdmConfig.asym, spdmUserConfig,
+                           spdm_app_lib::getSPDMConfigMap(
+                               spdm_app_lib::SPDMConfigIdentifier::asymHash)))
     {
         return spdmConfig;
     }
 
-    for (std::string& entry : reqasym)
+    if (!getField(map, "ReqAsym", spdmUserConfig))
     {
-        u32Data = 0;
-        if (!getValueFromName(asymValueStringTable, entry, u32Data))
-        {
-            return spdmConfig;
-        }
-        spdmConfig.reqasym = spdmConfig.reqasym | u32Data;
+        return spdmConfig;
     }
-
-    // get dhe
-    std::vector<std::string> dhe;
-    if (!getField(map, "Dhe", dhe))
+    if (!getSPDMConfigData(spdmConfig.reqasym, spdmUserConfig,
+                           spdm_app_lib::getSPDMConfigMap(
+                               spdm_app_lib::SPDMConfigIdentifier::asymHash)))
     {
         return spdmConfig;
     }
 
-    for (std::string& entry : dhe)
+    if (!getField(map, "Dhe", spdmUserConfig))
     {
-        u32Data = 0;
-        if (!getValueFromName(dheValueStringTable, entry, u32Data))
-        {
-            return spdmConfig;
-        }
-        spdmConfig.dhe = spdmConfig.dhe | u32Data;
+        return spdmConfig;
     }
-
-    // get aead
-    std::vector<std::string> aead;
-    if (!getField(map, "Aead", aead))
+    if (!getSPDMConfigData(spdmConfig.dhe, spdmUserConfig,
+                           spdm_app_lib::getSPDMConfigMap(
+                               spdm_app_lib::SPDMConfigIdentifier::dheValue)))
     {
         return spdmConfig;
     }
 
-    for (std::string& entry : aead)
+    if (!getField(map, "Aead", spdmUserConfig))
     {
-        u32Data = 0;
-        if (!getValueFromName(aeadValueStringTable, entry, u32Data))
-        {
-            return spdmConfig;
-        }
-        spdmConfig.aead = spdmConfig.aead | u32Data;
+        return spdmConfig;
     }
-    // get SlotCount
-    uint64_t slotCount;
+    if (!getSPDMConfigData(spdmConfig.aead, spdmUserConfig,
+                           spdm_app_lib::getSPDMConfigMap(
+                               spdm_app_lib::SPDMConfigIdentifier::aeadValue)))
+    {
+        return spdmConfig;
+    }
+
     if (!getField(map, "SlotCount", slotCount))
     {
         return spdmConfig;
     }
-    spdmConfig.slotcount = (uint32_t)slotCount;
-    // Get version
-    std::string version;
-    if (!getField(map, "Version", version))
-    {
-        return spdmConfig;
-    }
-    if (!getValueFromName(versionValueStringTable, version, spdmConfig.version))
-    {
-        return spdmConfig;
-    }
+    spdmConfig.slotcount = static_cast<uint32_t>(slotCount);
 
     return spdmConfig;
 }
